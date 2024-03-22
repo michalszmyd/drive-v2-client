a6aa52e2f2cb1dfca67823ca6953bccbd532f96b
Michal Szmyd <michal.szmyd@icloud.com>
2024-03-22 14:06:35 +0100

    Migrated to Vite

--- src/consts/settings.ts
+++ src/consts/settings.ts
@@ -1,9 +1,9 @@
 const SETTINGS = {
-  HOST_URL: process.env.REACT_APP_HOST_URL,
-  API_ORIGIN: process.env.REACT_APP_API_ORIGIN,
-  API_URL: process.env.REACT_APP_API_URL,
-  API_PREFIX: process.env.REACT_APP_API_PREFIX,
-  DEVELOPMENT: process.env.NODE_ENV !== 'development',
+  HOST_URL: import.meta.env.VITE_HOST_URL,
+  API_ORIGIN: import.meta.env.VITE_API_ORIGIN,
+  API_URL: import.meta.env.VITE_API_URL,
+  API_PREFIX: import.meta.env.VITE_API_PREFIX,
+  DEVELOPMENT: import.meta.env.NODE_ENV !== 'development',
 }
 
 export default SETTINGS;
